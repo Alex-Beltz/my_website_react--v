4aae3316f14cbff7abfb4d6d02fea8fa0823c66a
Alex Beltz <beltz.a@outlook.com>
2023-03-16 14:34:04 +0800

    Build Gallery and functionality

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,5 +1,49 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
+import "../styles/Gallery.css";
+import galleryImages from "./importImages.js";
 
-export default function Gallery() {
-  return <div>Gallery</div>;
-}
+const Gallery = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const modalRef = useRef(null);
+
+  const handleImageClick = (image) => {
+    setSelectedImage(image);
+  };
+
+  const handleModalClose = (event) => {
+    if (event.target === modalRef.current) {
+      setSelectedImage(null);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleModalClose);
+
+    return () => {
+      document.removeEventListener("mousedown", handleModalClose);
+    };
+  }, []);
+
+  return (
+    <div className="gallery">
+      {galleryImages.map((image, index) => (
+        <div
+          className="galleryImage"
+          key={index}
+          onClick={() => handleImageClick(image)}
+        >
+          <img src={image} alt={`gallery-image-${index}`} />
+        </div>
+      ))}
+      {selectedImage && (
+        <div className="galleryModal" ref={modalRef}>
+          <div className="modalContent">
+            <img src={selectedImage} alt="selected-image" />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Gallery;
