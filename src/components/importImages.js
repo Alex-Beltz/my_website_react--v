e318faefbb3419ee2a61fbb5daaa9726e29717d7
Alex Beltz <beltz.a@outlook.com>
2023-03-17 17:57:35 +0800

    Add image imports and images for gallery

---
+++ src/components/importImages.js
@@ -0,0 +1,64 @@
+// const importAll = (r) => r.keys().map(r);
+// const images = importAll(
+//   require.context("./galleryImages", false, /\.(png|jpe?g|svg)$/)
+// );
+// export default images;
+
+import image1 from "../galleryImages/1.jpg";
+import image2 from "../galleryImages/2.jpg";
+import image3 from "../galleryImages/3.jpg";
+import image4 from "../galleryImages/4.jpg";
+import image5 from "../galleryImages/5.jpg";
+import image6 from "../galleryImages/6.jpg";
+import image7 from "../galleryImages/7.jpg";
+import image8 from "../galleryImages/8.jpg";
+import image9 from "../galleryImages/9.jpg";
+import image10 from "../galleryImages/10.jpg";
+import image11 from "../galleryImages/11.jpg";
+import image12 from "../galleryImages/12.jpg";
+import image13 from "../galleryImages/13.jpg";
+import image14 from "../galleryImages/14.jpg";
+import image15 from "../galleryImages/15.jpg";
+import image16 from "../galleryImages/16.jpg";
+import image17 from "../galleryImages/17.jpg";
+import image18 from "../galleryImages/18.jpg";
+import image19 from "../galleryImages/19.jpg";
+import image20 from "../galleryImages/20.jpg";
+import image21 from "../galleryImages/21.jpg";
+import image22 from "../galleryImages/22.jpg";
+import image23 from "../galleryImages/23.jpg";
+import image24 from "../galleryImages/24.jpg";
+import image25 from "../galleryImages/25.jpg";
+import image26 from "../galleryImages/26.jpg";
+import image27 from "../galleryImages/27.jpg";
+
+const galleryImages = [
+  image1,
+  image2,
+  image3,
+  image4,
+  image5,
+  image6,
+  image7,
+  image8,
+  image9,
+  image10,
+  image11,
+  image12,
+  image13,
+  image14,
+  image15,
+  image16,
+  image17,
+  image18,
+  image19,
+  image20,
+  image21,
+  image22,
+  image23,
+  image24,
+  image25,
+  image26,
+  image27,
+];
+export default galleryImages;
